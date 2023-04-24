b8d75a802eea50c33cd9e221697e0081baca8c82
Ajmera Pavan Kumar <ajmerapavan20@gmail.com>
2023-04-24 23:57:11 +0530

    React-UseState

---
+++ src/topics/01-useState/01-error-example.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ErrorExample = () => {
+    let count = 0;
+
+    const handleClick = () => {
+        count += 1;
+        console.log(count);
+    }
+
+    return(
+        <div>
+            <h2>{count}</h2>
+            <button type='button' className="btn" onClick={handleClick}>increase</button>
+        </div>
+    );
+};
+
+export default ErrorExample;
